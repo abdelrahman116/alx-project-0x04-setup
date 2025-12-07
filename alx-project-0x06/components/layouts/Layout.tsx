6792f8411ee0e166e0d89b24a239528b95f1b777
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-12-07 22:47:40 +0200

    0x06

---
+++ alx-project-0x06/components/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import { LayoutProps } from "@/interface";
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
